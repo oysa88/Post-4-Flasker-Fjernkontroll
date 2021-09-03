07ac18963f1b7e076e70d53985198875cdf1f11f
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-09-03 23:33:14 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("spill", 3)
+    radio.sendValue("spill", 1)
+    radio.sendValue("spill", 4)
+    radio.sendValue("spill", 2)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(4)
